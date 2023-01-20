875185db2f0e825559d57f80937aca1b5146ba08
DESIREE VERANa <116412110+veranako@users.noreply.github.com>
2023-01-20 09:21:47 +0800

    Update and rename README.md to Battery.java

---
+++ Battery.java
@@ -0,0 +1,15 @@
+public class Battery {
+	private int percent;
+
+	public Battery(int percent) {
+		this.percent = percent;
+	}
+
+	public int getPercent() {
+		return percent;
+	}
+
+	public void setPercent(int percent) {
+		this.percent = percent;
+	}
+}
